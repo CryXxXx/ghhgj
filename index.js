946091bef42ad7cf8d4c6716c1870913df3beac7
CryXxXx <wertywerty2324@gmail.com>
2024-07-29 00:40:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
- const userName = window.Telegram.WebApp.initDataUnsafe.user.first_name;
- // document.querySelector('.nickname').textContent = userName; 
-  
-  const userAvatar = window.Telegram.WebApp.initDataUnsafe.user.photo_url;
- // document.querySelector('.circle-image').src = userAvatar; 
-  
-  const tgId = window.Telegram.WebApp.initDataUnsafe.user.id;
-  
-
-  fetch('https://f09f-89-22-177-227.ngrok-free.app/receive_tg_id', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ id: tgId })
-  })
-
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Сетевая ошибка при обращении к серверу');
-    }
-    return response.json();
-  })
-  .then(userData => {
-
-    document.querySelector('#tot-dep').textContent = userData.deposit;
-    document.querySelector('.profit-amount').textContent = userData.windeposit;
-    document.querySelector('#tot-trades').textContent = userData.trades;
-    document.querySelector('#week-dep').textContent = userData.deposit//доделать потом
-    document.querySelector('#week-trade').textContent = userData.trades;//доделать потом
-  })
-  .catch((error) => {
-    console.error('Ошибка:', error);
-  });
+ 
 
 MovCircle.addEventListener('click', () => {
   if (!hasMoved) { 
@@ -207,3 +174,38 @@ ValCircle.addEventListener('click', () => {
 
   }
 });
+
+const userName = window.Telegram.WebApp.initDataUnsafe.user.first_name;
+ // document.querySelector('.nickname').textContent = userName; 
+  
+  const userAvatar = window.Telegram.WebApp.initDataUnsafe.user.photo_url;
+ // document.querySelector('.circle-image').src = userAvatar; 
+  
+  const tgId = window.Telegram.WebApp.initDataUnsafe.user.id;
+  
+
+  fetch('https://f09f-89-22-177-227.ngrok-free.app/receive_tg_id', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id: tgId })
+  })
+
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Сетевая ошибка при обращении к серверу');
+    }
+    return response.json();
+  })
+  .then(userData => {
+
+    document.querySelector('#tot-dep').textContent = userData.deposit;
+    document.querySelector('.profit-amount').textContent = userData.windeposit;
+    document.querySelector('#tot-trades').textContent = userData.trades;
+    document.querySelector('#week-dep').textContent = userData.deposit//доделать потом
+    document.querySelector('#week-trade').textContent = userData.trades;//доделать потом
+  })
+  .catch((error) => {
+    console.error('Ошибка:', error);
+  });
